63af4f55b6ba1ec9b5d541fbf0032bf1b8f23101
César Augusto Silva Ruiz <Csilva@intergrupo.com>
2022-06-05 17:09:26 -0500

    Entity Framework usando Data annotation

---
+++ projectEF/Models/Categoria.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace projectEF.Models;
+
+[Table("Category")]
+public class Categoria
+{
+    [Key]
+    public Guid CategoriaId { get; set; }
+    [Required]
+    [MaxLength(150)]
+    public string Nombre { get; set; }
+    public string Descripcion { get; set; }
+    public virtual ICollection<Tarea> Tareas { get; set; }
+}
---
+++ projectEF/TareasContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using projectEF.Models;
+
+namespace projectEF;
+
+public class TareasContext : DbContext
+{
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Tarea> Tareas { get; set; }
+    public TareasContext(DbContextOptions<TareasContext> options) : base(options) { }
+}
